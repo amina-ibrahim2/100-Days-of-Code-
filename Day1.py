57fd1fce506ca24d78bf0080b761b8ba78370fa6
Amina Ibrahim <aminaibrahim@Abus-Air.attlocal.net>
2024-02-10 17:00:32 -0500

    Day1-Complete

---
+++ Day1.py
@@ -0,0 +1,66 @@
+# print( what to print) function --> output 
+# double quotes-text to print
+#Strings-pearl necklace, string of characters 
+
+print("Hello World")
+
+#print("Hello world!)
+
+# String Manipulation 
+
+print("Hello world!\nHello world!\nHello world!")
+
+# Concatenate Strings- combine different strings to be added to another string 
+# use the + to combine 2 strings
+#Think of strings as connected characters 
+# taking seperate strings and merging them into 1
+
+print("Hello" + "Amina")
+#print("Hello " + "Amina")
+#print("Hello" + " Amina")
+print("Hello" + " " "Amina")
+
+# Coding in Python-be careful with spaces or tabs inserted
+   #Indentation Error 
+ #print("Hello world!")
+
+# Python Input Function- input("prompt for the user") 
+input("What is your name? ")
+
+# What is your name and when I input name enter -the input is replaced by name and then concatenated to Hello and it all gets printed
+#print("Hello" + input("What is your name? "))
+
+# Variables 
+# Assigns whatever user inputs as name
+# We saved the data and used name as a variable 
+# It's like having a phone number in your phone book but no name. 
+# If we ever need it, we can refer to it as a variable
+# Can be changed or varied 
+
+#name = input("What is your name?")
+#print(name)
+
+name = "Jackie"
+print(name)
+
+name = "Amina"
+print(name)
+
+# print(len(input("What is your name?"))) 
+name = input("What is your name?") # Name is what the user set
+length = len(name) # variable to set the legnth 
+print(length)
+
+
+# Naming Variables -make your code readable- it makes sense 
+# No spaces in a variable -one unit, use _ to seperate or numbers(at the end)
+# Don't use functions as names for variables- bad practice 
+
+n = "Muna"
+print(name)
+
+user_name = "Muna"
+print(user_name)
+
+name1 = "Amina"
+print(name)
